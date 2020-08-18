15e63ec6f09a6c923d2c3774e802a0b77c34aae4
Allison Nunes <allison.julio@hotmail.com>
2020-08-18 15:41:30 -0300

    Change behavior app.component

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './views/auth/auth.component';
+import { RegisterComponent } from './views/register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: AuthComponent,
+    pathMatch: 'full',
+    data: { title: 'Autenticação' },
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    pathMatch: 'full',
+    data: { title: 'Registrar' },
+  },
+  { path: '**', redirectTo: 'login' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'track-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'trackco';
+  constructor(private router: Router, private titleService: Title) {
+    this.titleService.setTitle('Some title');
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        const title = this.getTitle(
+          this.router.routerState,
+          this.router.routerState.root
+        ).join(' | ');
+        this.titleService.setTitle(title);
+      });
+  }
+  private getTitle(state, parent): any[] {
+    const data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
+    if (state && parent) {
+      data.push(...this.getTitle(state, state.firstChild(parent)));
+    }
+    return data;
+  }
 }
