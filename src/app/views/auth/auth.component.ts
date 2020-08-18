f5e563a5680079d978211d1e0bd53faf8cf0e41b
Allison Nunes <allison.julio@hotmail.com>
2020-08-18 15:40:58 -0300

    Create auth component - mananger register and login

---
+++ src/app/views/auth/auth.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Component({
+  selector: 'track-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
+})
+export class AuthComponent implements OnInit {
+  public isLogin: boolean;
+  constructor(private loginService: AuthService) {}
+  loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+  ngOnInit(): void {}
+  login(): void {
+    const { email, password } = this.loginForm.value;
+    this.loginService
+      .login(email, password)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+  changeRegister(): void {
+    this.isLogin = !this.isLogin;
+  }
+}
