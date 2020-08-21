d0f3af6c7c8f7008e55ad958b179decd6ccfa352
Allison Nunes <allison.julio@hotmail.com>
2020-08-21 10:23:53 -0300

    Config angular json to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+};
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCOWahJCZDxPZxlAGMAizZMqA0aeBa-I_c',
+  authDomain: 'trackto-challenge.firebaseapp.com',
+  databaseURL: 'https://trackto-challenge.firebaseio.com',
+  projectId: 'trackto-challenge',
+  storageBucket: 'trackto-challenge.appspot.com',
+  messagingSenderId: '674937246723',
+  appId: '1:674937246723:web:a5f7288cf7a358eb6e6a36',
+  measurementId: 'G-4RJ3WHX2BG',
 };
